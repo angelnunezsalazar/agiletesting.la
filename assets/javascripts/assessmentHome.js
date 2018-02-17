eeb7e104a378d57e8fd132b493d354f5e12199af
Snahider <snahider@gmail.com>
2018-02-17 03:24:36 -0500

    js refactoring

--- assets/javascripts/assessmentHome.js
+++ assets/javascripts/assessmentHome.js
@@ -1,90 +1,133 @@
+var SmoothScrolling = function () {
 
-$(document).ready(function() {
-  // Smooth scrolling
-  $(function() {
+  function enableForAllLinks() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
+  
         if (target.length) {
           $('html, body').animate({scrollTop: target.offset().top}, 1000, 'easeInOutExpo');
           return false;
         }
       }
     });
-  });
+  };
 
-  //Scroll Top link
-  $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-      $('.scrolltop').fadeIn();
-    } else {
-      $('.scrolltop').fadeOut();
-    }
-  });
-  $('.scrolltop, #logo a').click(function(){
-    $("html, body").animate({
-      scrollTop: 0
-    }, 1000, 'easeInOutExpo');
-    return false;
-  });
+  return {
+    enableForAllLinks: enableForAllLinks,
+  }
+};
 
-  //Analytics
-  var analyticsIsEnabled= window.location.hostname!="localhost";
-  if(analyticsIsEnabled)
-  {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-11277405-2');
+var ScrollToTopLink = function (linkElement) {
 
-    $(document).ready(function () {
-      gtag('event', '0. Ingresa al Home', {
-        'event_category': 'Home Buttons'
-      });
+  function enable() {
+    onClick();
+    fadeOnTop();
+  };
 
-      $('#comienza-ahora').click(function (e) { 
-        e.preventDefault();
-        gtag('event', '1. Ir a Agile Testing (Comienza Ahora)', {
-          'event_category': 'Home Buttons'
-        });
-      });
-      $('#assessment-que-es').click(function (e) { 
-        e.preventDefault();
-        gtag('event', '2. Ir a Assessment Que Es', {
-          'event_category': 'Home Buttons'
-        });
-      });
-      $('#evalua-tu-madurez').click(function (e) { 
-        e.preventDefault();
-        gtag('event', '3. Ir a Formulario Inicio', {
-          'event_category': 'Home Buttons'
-        });
-      });
+  function onClick(){
+    $(linkElement).click(function(){
+      $("html, body").animate({
+        scrollTop: 0
+      }, 1000, 'easeInOutExpo');
+      return false;
     });
   }
 
-  //Form Validation
-  $("#assessment-form" ).validate( {
-    errorElement: "small",
-    errorPlacement: function ( error, element ) {
-      error.addClass( "form-text" );
-      error.insertAfter( element );
-    },
-    highlight: function ( element, errorClass, validClass ) {
-      $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
-    },
-    unhighlight: function (element, errorClass, validClass) {
-      $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
-    },
-    submitHandler: function (form) {
-      if(analyticsIsEnabled){
-        gtag('event', '4. Comenzar Assessment', {
-          'event_category': 'Home Buttons'
+  function fadeOnTop(){
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+        $(linkElement).fadeIn();
+      } else {
+        $(linkElement).fadeOut();
+      }
+    });
+  }
+  return {
+    enable: enable,
+  }
+};
+
+var Analytics = function () {  
+
+  var analyticsIsEnabled = window.location.hostname!="localhost";
+  configure();
+
+  function configure () {  
+    if(analyticsIsEnabled){
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-11277405-2');
+    }
+  }
+
+  function addEventOnClick(element,action,category) {
+    $(element).click(function (e) { 
+      e.preventDefault();
+      sendEvent(action,category);
+    });
+  }
+
+  function sendEvent(action,category){
+    if(analyticsIsEnabled){
+      gtag('event', action, {
+        'event_category': category
+      });
+    }
+  }
+  return {
+    analyticsIsEnabled: analyticsIsEnabled,
+    addEventOnClick: addEventOnClick,
+    sendEvent: sendEvent
+  }
+};
+
+$.validator.setDefaults({ 
+  errorElement: "small",
+  errorPlacement: function ( error, element ) {
+    error.addClass( "form-text" );
+    error.insertAfter( element );
+  },
+  highlight: function ( element, errorClass, validClass ) {
+    $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
+  },
+  unhighlight: function (element, errorClass, validClass) {
+    $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
+  }
+});
+
+$(document).ready(function() { 
+    /*
+    $.fn.isInViewport = function() {
+      var elementTop = $(this).offset().top;
+      var elementBottom = elementTop + $(this).outerHeight();
+      var viewportTop = $(window).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+      return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+    gtag('event', '0. Home', {
+      'event_category': 'Scroll'
+    });
+    
+    $(window).on('resize scroll', function() {
+      if ($('#agile-testing').isInViewport()) {
+        gtag('event', '1. Agile Testing', {
+          'event_category': 'Scroll'
         });
       }
-      form.submit();
-    }
-  });
+      if ($('#assessment-que-es').isInViewport()) {
+        gtag('event', '2. Assessment Que Es', {
+          'event_category': 'Scroll'
+        });
+      }
+      if ($('#assessment-start').isInViewport()) {
+        gtag('event', '3. Formulario Inicio', {
+          'event_category': 'Scroll'
+        });
+      }
+    });
+    */
 });
